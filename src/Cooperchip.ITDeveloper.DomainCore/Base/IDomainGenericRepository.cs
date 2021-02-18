c9e684a4a94c5b56fd3a1bbb73592b410fdb1c3a
Carlos A Santos <carlos.itdeveloper@gmail.com>
2021-02-17 22:03:01 -0300

    Interfce com contrato do Repositório Genérico criada, falta implementar no Repository Laeyr com EF.

---
+++ src/Cooperchip.ITDeveloper.DomainCore/Base/IDomainGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Cooperchip.ITDeveloper.DomainCore.Base
+{
+    public interface IDomainGenericRepository<TEntity, TKey> : IDisposable where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> SelecionarTodos(Expression<Func<TEntity, bool>> quando = null);
+        Task<TEntity> SelecionarPorId(TKey id);
+        Task Inserir (TEntity obj);
+        Task Atualizar(TEntity obj);
+        Task Excluir(TEntity obj);
+        Task ExcluirPorId(TKey id);
+    }
+}
