2e50cc175a721ad048da3297b9e60e6e9bbea705
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-20 20:50:44 -0300

    Rebamed Solution and erased comments of the EntityBase

--- src/Cooperchip.ITDeveloper.Domain/Entities/EntityBase.cs
+++ src/Cooperchip.ITDeveloper.Domain/Entities/EntityBase.cs
@@ -8,24 +8,7 @@ namespace Cooperchip.ITDeveloper.Domain.Entities
         {
             this.Id = Guid.NewGuid();
         }
-
         public Guid Id { get; set; }
-
-        // DataCadastro
-        // DataModificacao
-        // UserChange
-
-        // Criar Comparações entre entidades
-
-
-        // -------------------------------------- Para refletir
-        // Maca = OutraMaca ????
-
-        // maca = new Maca()  => Instancia de uma Maca
-
-        // Pessoa xpto = new {id=1, nome=Carlos, cpf=38487432943};
-        // Pessoa xpto2 = new {id=2, nome=Marcos, cpf=77487432943};
-
 
     }
 }
